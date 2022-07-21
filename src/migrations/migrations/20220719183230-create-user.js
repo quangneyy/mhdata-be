b8b09e3c5f5c634bd8cab192219ffc37c4776fd2
QuangNey <quangney12602@gmail.com>
2022-07-21 23:47:27 +0700

    naming model

--- src/migrations/migrations/20220719183230-create-user.js
+++ src/migrations/migrations/20220719183230-create-user.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('User', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -30,7 +30,7 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('User');
   }
 };
 
