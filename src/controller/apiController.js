c9d5d1f92743435c5f1c3046ca0e000b824d0deb
QuangNey <quangney12602@gmail.com>
2022-07-24 16:18:46 +0700

    register-next

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -1,3 +1,5 @@
+import loginRegisterService from '../service/loginRegisterService';
+
 const testApi = (req, res) => {
     return res.status(200).json({
         message: 'ok',
@@ -5,8 +7,40 @@ const testApi = (req, res) => {
     })
 }
 
-const handleRegister = (req, res) => {
-    console.log(">>> call me", req.body)
+const handleRegister = async (req, res) => {
+    try {
+        // req.body: email, phone, username, password
+        if(!req.body.email || !req.body.phone || !req.body.password) {
+            return res.status(200).json({
+                EM: 'Missing required parameters', // error message
+                EC: '1', // error code
+                DT: '', // date
+            })
+        }
+        if (req.body.password && req.body.password.length < 4) {
+            return res.status(200).json({
+                EM: 'Your password must have more than 3 letters', // error message
+                EC: '1', // error code
+                DT: '', // date
+            })
+        }
+
+        // service: create user
+        let data = await loginRegisterService.registerNewUser(req.body)
+
+        return res.status(200).json({
+            EM: data.EM, 
+            EC: data.EC,
+            DT: '',
+        })
+
+    } catch (e) {
+        return res.status(500).json({
+            EM: 'error from server', // error message
+            EC: '-1', // error code
+            data: '', // date
+        })
+    }
 }
 
 module.exports = {
