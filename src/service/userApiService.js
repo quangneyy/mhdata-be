4eb225779c6e695deab56fc870e7365074e479b5
QuangNey <quangney12602@gmail.com>
2022-08-02 17:03:54 +0700

    pagination

--- src/service/userApiService.js
+++ src/service/userApiService.js
@@ -4,8 +4,6 @@ const getAllUser = async () => {
     try {
         let users = await db.User.findAll();
         if (users) {
-            console.log(">>> check users: ", users);
-            // let data = users.get({ plain: true });
             return {
                 EM: 'get data success',
                 EC: 0,
@@ -28,21 +26,79 @@ const getAllUser = async () => {
     }
 }
 
-const createNewUser = () => {
-    
+const getUserWithPagination = async (page, limit) => {
+    try {
+        let offset = (page - 1) * limit;
+
+        const { count, rows } = await db.User.findAndCountAll({
+            offset: offset,
+            limit: limit,
+        })
+
+        let totalPages = Math.ceil(count / limit);
+        let data = {
+            totalRows: count,
+            totalPages: totalPages,
+            users: rows
+        }
+
+        return {
+            EM: 'fetch ok',
+            EC: 0,
+            DT: data
+        }
+
+    } catch (e) {
+        console.log(e);
+        return {
+            EM: 'something wrongs with services',
+            EC: 1,
+            DT: []
+        }
+    }
 }
 
-const updateUser = () => {
-    
+const createNewUser = async (data) => {
+    try {
+        
+    } catch (e) {
+        console.log(e);
+    }
 }
 
-const deleteUser = () => {
-    
+const updateUser = async (data) => {
+    try {
+        let user = await db.User.findOne({
+            where: { id: data.id }
+        })
+        if (user) {
+            //update
+            user.save({
+
+            })
+        } else {
+            //not found
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+const deleteUser = async (id) => {
+    try {
+        await db.User.deleteUser({
+            where: { id: id }
+        })
+
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 module.exports = {
     getAllUser,
     createNewUser,
     updateUser,
-    deleteUser
+    deleteUser,
+    getUserWithPagination
 }
