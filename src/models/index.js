64d100f3ae3b76d2f13cd1923e6508e0add8df2a
QuangNey <quangney12602@gmail.com>
2022-08-12 00:46:24 +0700

    comment-connect

--- src/models/index.js
+++ src/models/index.js
@@ -8,6 +8,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
+// connect database
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
@@ -15,6 +16,7 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
+// add add models
 fs
   .readdirSync(__dirname)
   .filter(file => {
